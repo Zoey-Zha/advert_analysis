c5e836dd8345e96a81edacf8126b920d24dfd30a
Zoey-Zha <xinzha92@gmail.com>
2022-06-11 00:33:08 +0800

    try SQLContext:Saprk SQL 1.x版本用的，2.x使用SparkSession作为入口点

---
+++ src/main/scala/com/zoey/chapter04/SQLContextApp.scala
@@ -0,0 +1,24 @@
+package com.zoey.chapter04
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
+
+object SQLContextApp {
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setMaster("local").setAppName("SQLContextApp")
+
+    val sc = new SparkContext(sparkConf)
+
+    val sc1 = new SQLContext(sc)
+
+    val df = sc1.read.text("file:///Users/xinzha/Documents/02 Projects/advert_analysis/data/people.txt")
+
+    // df.printSchema()
+    df.show(false)
+
+    sc.stop()
+
+  }
+
+}
--- src/main/scala/com/zoey/chapter04/SparkSessionApp.scala
+++ src/main/scala/com/zoey/chapter04/SparkSessionApp.scala
@@ -1,6 +1,24 @@
 package com.zoey.chapter04
 
+import org.apache.spark.sql.SparkSession
+
 object SparkSessionApp {
 
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName("SparkSessionApp")
+      .getOrCreate()
+
+    val df = spark.read.text("file:///Users/xinzha/Documents/02 Projects/advert_analysis/data/people.txt")
+
+    // df.show()
+    df.printSchema()
+
+    // TODO 进行其他业务处理
+
+    spark.stop()
+
+  }
 
 }
