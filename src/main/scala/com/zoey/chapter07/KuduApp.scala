2d632b2f5ccf0630a6f49081e402048b3d637f92
Zoey-Zha <xinzha92@gmail.com>
2022-06-13 01:38:23 +0800

    Kudu APIs: 但我感觉操作好麻烦啊

--- src/main/scala/com/zoey/chapter07/KuduApp.scala
+++ src/main/scala/com/zoey/chapter07/KuduApp.scala
@@ -1,15 +1,15 @@
 package com.zoey.chapter07
 
+import org.apache.derby.impl.sql.compile.TableName
 import org.apache.kudu.ColumnSchema.ColumnSchemaBuilder
 import org.apache.kudu.{ColumnSchema, Schema, Type}
-import org.apache.kudu.client.{CreateTableOptions, KuduClient}
+import org.apache.kudu.client.{CreateTableOptions, Insert, KuduClient, KuduScanner, KuduSession, KuduTable, PartialRow, RowResult, RowResultIterator, Update}
 
 import java.util
 import scala.collection.JavaConverters._
 import collection.mutable._
 
 object KuduApp {
-
 
   def main(args: Array[String]): Unit = {
     val MASTER_ADDRESS: String = "hadoop000"
@@ -18,12 +18,23 @@ object KuduApp {
 
     val tableName = "test"
 
-//    val scalaList: java.util.List[Int] = List(1,2,3).asJava
-//    val jul: java.util.List[Int] = List(1, 2, 3).asJava
+    // 创建表
+    // createTable(client, tableName)
 
+    // 删除表
+    // deleteTable(client, tableName)
 
+    // 插入数据
+    //  inseartRows(client, tableName)
 
-    createTable(client, tableName)
+    queryTable(client, tableName)
+    print("+++++++++++++++++++++++")
+
+    updateTable(client, tableName)
+
+    print("+++++++++++++++++++++++")
+
+    queryTable(client, tableName)
 
     client.close()
 
@@ -37,7 +48,7 @@ object KuduApp {
     import org.apache.kudu.ColumnSchema
     val columns = new util.ArrayList[ColumnSchema](2)
     columns.add(new ColumnSchema.ColumnSchemaBuilder("word", Type.STRING).key(true).build)
-    columns.add(new ColumnSchema.ColumnSchemaBuilder("cnt", Type.INT16).build)
+    columns.add(new ColumnSchema.ColumnSchemaBuilder("cnt", Type.INT32).build)
 
 //    val columns: java.util.List[ColumnSchema] = List(
 //      new ColumnSchemaBuilder("word", Type.STRING).build(),
@@ -57,6 +68,53 @@ object KuduApp {
 
     client.createTable(tableName, schema, tableOptions)
   }
+
+  def deleteTable(client: KuduClient, tableName: String) = {
+
+      client.deleteTable(tableName)
+  }
+
+
+  def inseartRows(client: KuduClient, tableName: String) = {
+    val table: KuduTable = client.openTable(tableName)
+    val session: KuduSession = client.newSession()
+
+    for (i <- 1 to 10) {
+      val insert: Insert = table.newInsert()
+      val row: PartialRow = insert.getRow
+      row.addString("word", s"alix-$i")
+      row.addInt("cnt",100+i)
+
+      session.apply(insert)
+    }
+  }
+
+
+  def updateTable(client: KuduClient, tableName: String) = {
+    val table: KuduTable = client.openTable(tableName)
+    val session: KuduSession = client.newSession()
+
+    val update: Update = table.newUpdate()
+    val row: PartialRow = update.getRow()
+    row.addString("word", "alix-8")
+    row.addInt("cnt",10086)
+
+    session.apply(update)
+  }
+
+  def queryTable(client: KuduClient, tableName: String) ={
+    val table: KuduTable = client.openTable(tableName)
+    val scanner: KuduScanner = client.newScannerBuilder(table).build()
+
+    while(scanner.hasMoreRows()) {
+      val iterator: RowResultIterator = scanner.nextRows()
+      while(iterator.hasNext) {
+        val result: RowResult = iterator.next()
+        println(result.getString("word") + "=>" + result.getInt("cnt"))
+      }
+    }
+  }
+
 
 
 }
